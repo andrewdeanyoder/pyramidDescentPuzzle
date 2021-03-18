64db93f517cbf71ac25df21a56697b75901223fe
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2021-03-17 23:06:14 -0500

    added tests for findChildren

--- pyramidDescent.test.js
+++ pyramidDescent.test.js
@@ -28,13 +28,43 @@ describe('findRow', () => {
 
 describe('findChildren', () => {
   test('returns an array when children exist', () => {
-    expect(Array.isArray(findChildren(0, [1, 2, 3]))).toBe(true);
+    expect(Array.isArray(findChildren(0, 3))).toBe(true);
+  });
+
+  test('result array has two elements', () => {
+    const result = findChildren(0, 3);
+    expect(result.length).toBe(2);
+    expect(result[0]).not.toBe(undefined);
+    expect(result[1]).not.toBe(undefined);
   });
 
   test('returns null when children do not exist', () => {
-    expect(findChildren(0, [1])).toBe(null);
-    expect(findChildren(3, [1, 2, 3])).toBe(null);
+    expect(findChildren(0, 1)).toBe(null);
+    expect(findChildren(3, 3)).toBe(null);
+    expect(findChildren(1, 1)).toBe(null);
   });
+
+  test('returns null when array is empty', () => {
+    expect(findChildren(0, 0)).toBe(null);
+  });
+
+  test('returns the children in a small pyramid', () => {
+    expect(findChildren(0, 3)).toEqual([1, 2]);
+    expect(findChildren(1, 6)).toEqual([3, 4]);
+    expect(findChildren(2, 6)).toEqual([4, 5]);
+  });
+
+  test('returns the children in a large pyramid', () => {
+    expect(findChildren(5049, 5151)).toEqual([5149, 5150]);
+    expect(findChildren(1275, 5151)).toEqual([1326, 1327]);
+  });
+
+  test('returns children correctly in an incomplete pyramid', () => {
+    const pyramidSize = 11;
+    expect(findChildren(8, pyramidSize)).toBe(null);
+    expect(findChildren(4, pyramidSize)).toEqual([7, 8]);
+  });
+
 });
 
 describe('solvePyramid', () => {
